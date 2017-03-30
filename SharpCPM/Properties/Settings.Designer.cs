e53cade1ec67dd3b25ac30c69c9f48fcd6725283
Kenny Fryar-Ludwig <fryarludwig@gmail.com>
2017-03-30 16:39:47 -0600

    Synchronization push - no new features

--- SharpCPM/Properties/Settings.Designer.cs
+++ SharpCPM/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SharpCPM.Properties {
+namespace CPMClient.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
